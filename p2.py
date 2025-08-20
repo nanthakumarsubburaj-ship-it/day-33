9d0b4b93ebb45244b24052e9ade93990782bb8e0
nanthakumarsubburaj-ship-it <nanthakumarsubburaj@gmail.com>
2025-08-20 20:34:00 +0400

    Add files via upload

---
+++ p2.py
@@ -0,0 +1,5 @@
+import numpy as np
+arr = np.array([10, 20, 30, 40])
+print("Array:", arr)
+print("Mean:", np.mean(arr)) # Calculate Mean
+print("Standard Deviation:", np.std(arr)) # Standard deviation
